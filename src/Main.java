8be381d17d5606f975f7f09af8a04a4035b729c7
veer077 <anirudhveer077@gmail.com>
2017-09-12 12:28:46 +0530

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
