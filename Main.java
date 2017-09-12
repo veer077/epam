8671650d9244886d88fc74250b26bb75f072cbb9
veer077 <anirudhveer077@gmail.com>
2017-09-12 12:22:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		
 		//You may test that your code works find here
 		//Please check that your code works and has no 
